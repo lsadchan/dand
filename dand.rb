4f3367969570f4f5d4fb8de1ed08389b32cc8a2c
Damian Chan <d.chan@2009.ljmu.ac.uk>
2012-10-23 13:11:42 +0100

    Dungeons and Dragons

---
+++ dand.rb
@@ -0,0 +1,13 @@
+player_1 = "aaron"
+player_2 = "Dave"
+player_3 = "John"
+p1_health = 60
+p2_health = 80
+p3_health = 100
+
+
+puts "#{player_1}'s health is #{p1_health * 3}."
+puts "#{player_1}'s health is now #{p1_health / 9.0}.\n\r"
+
+puts "Players:\n\t#{player_1}\n\t#{player_2}\n\t#{player_3}\n\r"
+puts "Players Health:\n\t#{player_1}: #{p1_health}\n\t#{player_2}: #{p2_health}\n\t#{player_3}: #{p3_health}"
